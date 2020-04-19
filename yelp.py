83e5eb82f48e31edd9caf3773b6808225bc217ad
Jessica <yutsuc@gmail.com>
2020-04-19 02:13:33 -0700

    feat: Add insert cafe to database

--- yelp.py
+++ yelp.py
@@ -1,13 +1,44 @@
-import sys
 import requests
+import sqlite3
+import sys
 from urllib.error import HTTPError
 from urllib.parse import quote
 from urllib.parse import urlencode
 
+# Database variables
+CONN = sqlite3.connect('./YelpCafe.sqlite')
+CUR = CONN.cursor()
+
 # API variables
-API_KEY= "EKJ9TZaYDzW-wnqgcQxNVVQQwz-K624lEH_Cwj1DI7NvHZm16P6P0YMsfvE2Jm4a1h2GWGwZlSikgo45BzGJXfxLdPtWBrlq6cJYMfLsjZrUq8XZZOJogZ65kaaYXnYx" 
+API_KEY= 'EKJ9TZaYDzW-wnqgcQxNVVQQwz-K624lEH_Cwj1DI7NvHZm16P6P0YMsfvE2Jm4a1h2GWGwZlSikgo45BzGJXfxLdPtWBrlq6cJYMfLsjZrUq8XZZOJogZ65kaaYXnYx'
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
+
+def insertDataToDB(table, data):
+    if table == 'cafe':
+        insert = 'INSERT INTO Cafe VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+    elif table == 'category':
+        insert = 'INSERT INTO Category VALUES (NULL, ?, ?)'
+    elif table == 'cafe_category':
+        insert = 'INSERT INTO Cafe_Category VALUES (?, ?)'
+  
+    CUR.execute(insert, data)
+    CONN.commit()
+
+def insertCategory(categories):
+    return ""
+    
+def insertCafes(cafes):
+    # TODO: check if exist before insert
+    for c in cafes:
+        cafe_values = [c['id'], c['name'], c['rating'], c['review_count'], c['location']['state'],
+            c['location']['city'], ', '.join(c['location']['display_address']), c['location']['zip_code'], c['display_phone'], c['url']]
+        insertCategory(c['categories'])
+        insertDataToDB('cafe', cafe_values)
+    test = CUR.execute('SELECT * FROM Cafe')
+    rows = CUR.fetchall()
+    return rows
+    
 
 # Sends request to Yelp Fusion API, Business Endpoint
 def request(url_params=None):
@@ -32,7 +63,8 @@ def searchByLocation(location):
     results = request(url_params)
     businesses = results.get('businesses')
     total_results = results.get('total')
-
+    insertCafes(businesses)
+    
     return businesses
 
 def main():
